8bddac9989fc552a1201779143de48af4b7e05d6
shinningyu55688 <shinningyu55688@gmail.com>
2022-03-18 16:37:34 +0800

    Removed The Swear Word

--- api/controllers/home/index.js
+++ api/controllers/home/index.js
@@ -17,10 +17,14 @@ module.exports = {
   },
 
 
-  fn: async function (inputs) {
+  fn: async function () {
 
     // All done.
-    return "motherfuckers";
+    return {
+      status:"success",
+      msg:"Hello! To get started, please visit https://github.com/tonyyu-taipei/TaiwanSportsCenter-Charts_SAILS-SERVER",
+      msgCH:"歡迎瀏覽！如需更多資訊，請前往： https://github.com/tonyyu-taipei/TaiwanSportsCenter-Charts_SAILS-SERVER"
+    };
 
   }
 
